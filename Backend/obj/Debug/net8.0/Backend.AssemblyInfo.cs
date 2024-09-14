2a289343d0cbf64fad99033595f76757a25fcd99
Mateusz Placha <s25946@pjwstk.edu.pl>
2024-09-14 23:51:17 +0200

    add sometin

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fb054f8fde3d437c77f6bfae8ff7b9f3fe5e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411d03faf402efa28281681b25884e5629cf6416")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
