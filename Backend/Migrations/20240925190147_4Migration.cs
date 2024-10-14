914cddc76621a0a1e09eb4a77598a3b30a9c0f5a
Mateusz Placha <s25946@pjwstk.edu.pl>
2024-10-14 09:51:22 +0200

    Usunicie plików z ledzenia, które s w .gitignore

---
+++ Backend/Migrations/20240925190147_4Migration.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class _4Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teams_Conversations_ConversationId",
+                table: "Teams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teams_ConversationId",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "ConversationId",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Teams");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "Conversations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_TeamId",
+                table: "Conversations",
+                column: "TeamId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Conversations_Teams_TeamId",
+                table: "Conversations",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Conversations_Teams_TeamId",
+                table: "Conversations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Conversations_TeamId",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Conversations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ConversationId",
+                table: "Teams",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Teams",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_ConversationId",
+                table: "Teams",
+                column: "ConversationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teams_Conversations_ConversationId",
+                table: "Teams",
+                column: "ConversationId",
+                principalTable: "Conversations",
+                principalColumn: "Id");
+        }
+    }
+}
