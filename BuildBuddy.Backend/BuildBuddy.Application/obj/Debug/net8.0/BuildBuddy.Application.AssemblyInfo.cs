915aaa6545203176411062f4c380d28e8461d4a1
Mateusz Placha <s25946@pjwstk.edu.pl>
2025-01-04 20:08:31 +0100

    XD

--- BuildBuddy.Backend/BuildBuddy.Application/obj/Debug/net8.0/BuildBuddy.Application.AssemblyInfo.cs
+++ BuildBuddy.Backend/BuildBuddy.Application/obj/Debug/net8.0/BuildBuddy.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildBuddy.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9198a6fb62728585429fdad017d68756f3d9cc56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b799499f944f739593e3c95aadb3eeaec753c1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildBuddy.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildBuddy.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
