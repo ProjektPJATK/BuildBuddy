9cd3daba224887e00a0ce56524d03f41ff102080
IgorPodsiadlo <s24394@pjwstk.edu.pl>
2025-01-08 01:33:39 +0100

    cashed profile

--- mobile/lib/features/profile/views/user_profile_screen.dart
+++ mobile/lib/features/profile/views/user_profile_screen.dart
@@ -19,7 +19,8 @@ class UserProfileScreen extends StatefulWidget {
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
   final UserService _userService = UserService();
-  User? userProfile; // Nullable to handle null gracefully
+  User? userProfile; // Local cache for user profile
+  String? userImageUrl; // Cache for the user's image
 
   @override
   void initState() {
@@ -27,13 +28,18 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     _loadUserProfile();
   }
 
-  void _loadUserProfile() {
+  // Load user profile with cache check
+  void _loadUserProfile() async {
     final profileBloc = context.read<ProfileBloc>();
-    profileBloc.add(FetchProfileFromCacheEvent());
 
-    Future.delayed(const Duration(milliseconds: 500), () {
-      profileBloc.add(FetchProfileEvent());
-    });
+    // Check if the profile is already cached
+    if (userProfile == null) {
+      profileBloc.add(FetchProfileFromCacheEvent());
+
+      Future.delayed(const Duration(milliseconds: 500), () {
+        profileBloc.add(FetchProfileEvent());
+      });
+    }
   }
 
   void _logout() {
@@ -45,8 +51,17 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       context: context,
       builder: (_) => EditProfileDialog(
         user: profile,
-        onSave: (updatedProfile) {
+        onSave: (updatedProfile) async {
+          setState(() {
+            userProfile = updatedProfile;  // Update local cache
+          });
           context.read<ProfileBloc>().add(EditProfileEvent(updatedProfile));
+
+          // Reload the user image after profile update
+          final updatedImageUrl = await _userService.getUserImage(profile.id);
+          setState(() {
+            userImageUrl = updatedImageUrl;  // Update image cache
+          });
         },
       ),
     );
@@ -71,23 +86,26 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                     color: AppStyles.transparentWhite,
                     child: BlocBuilder<ProfileBloc, ProfileState>(
                       builder: (context, state) {
-                        if (state is ProfileLoading) {
+                        if (state is ProfileLoading && userProfile == null) {
                           return const Center(child: CircularProgressIndicator());
                         } else if (state is ProfileLoaded) {
-                          userProfile = state.profile; // Cache the loaded profile
+                          userProfile = state.profile;  // Cache profile
                           return _buildProfileContent(userProfile!);
                         } else if (state is LogoutSuccess) {
                           Future.microtask(() {
                             Navigator.pushReplacementNamed(context, '/');
                           });
                           return const SizedBox.shrink();
-                        } else if (state is ProfileError) {
+                        } else if (state is ProfileError && userProfile == null) {
                           return Center(
                             child: Text(
                               state.message,
                               style: const TextStyle(color: Colors.white),
                             ),
                           );
+                        } else if (userProfile != null) {
+                          // If the cached profile exists, display it
+                          return _buildProfileContent(userProfile!);
                         }
                         return const SizedBox.shrink();
                       },
@@ -106,73 +124,90 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   }
 
   Widget _buildProfileContent(User profile) {
-  return SingleChildScrollView(
-    child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 30),
-          FutureBuilder<String>(
-            future: _userService.getUserImage(profile.id),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircleAvatar(
-                  radius: 50,
-                  child: CircularProgressIndicator(),
-                );
-              } else if (snapshot.hasError || snapshot.data!.isEmpty) {
-                return const CircleAvatar(
-                  radius: 50,
-                  child: Icon(Icons.person, size: 50),
-                );
-              } else {
-                return CircleAvatar(
-                  radius: 50,
-                  backgroundImage: NetworkImage(snapshot.data!),
-                );
-              }
-            },
-          ),
-          const SizedBox(height: 20),
-          Text(
-            '${profile.name} ${profile.surname}',
-            style: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            userImageUrl != null
+                ? CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(userImageUrl!),
+                  )
+                : FutureBuilder<String>(
+                    future: _loadUserImage(profile.id),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircleAvatar(
+                          radius: 50,
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError || snapshot.data!.isEmpty) {
+                        return const CircleAvatar(
+                          radius: 50,
+                          child: Icon(Icons.person, size: 50),
+                        );
+                      } else {
+                        userImageUrl = snapshot.data;  // Cache the image URL
+                        return CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(userImageUrl!),
+                        );
+                      }
+                    },
+                  ),
+            const SizedBox(height: 20),
+            Text(
+              '${profile.name} ${profile.surname}',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-          ProfileItem(icon: Icons.email, title: profile.email),
-          ProfileItem(icon: Icons.phone, title: profile.telephoneNr),
-          ProfileItem(icon: Icons.language, title: profile.preferredLanguage.toUpperCase()),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => _showEditProfileDialog(profile),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
+            const SizedBox(height: 10),
+            ProfileItem(icon: Icons.email, title: profile.email),
+            ProfileItem(icon: Icons.phone, title: profile.telephoneNr),
+            ProfileItem(icon: Icons.language, title: profile.preferredLanguage.toUpperCase()),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _showEditProfileDialog(profile),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                  ),
+                  child: const Text('EDYTUJ PROFIL'),
                 ),
-                child: const Text('EDYTUJ PROFIL'),
-              ),
-              const SizedBox(width: 20),
-              ElevatedButton(
-                onPressed: _logout,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _logout,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text('WYLOGUJ SIĘ'),
                 ),
-                child: const Text('WYLOGUJ SIĘ'),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
+  // Load user image from cache or API
+  Future<String> _loadUserImage(int userId) async {
+    if (userImageUrl != null) return userImageUrl!;
+    try {
+      final imageUrl = await _userService.getUserImage(userId);
+      userImageUrl = imageUrl;  // Cache the result
+      return imageUrl;
+    } catch (e) {
+      return '';  // Return empty if loading fails
+    }
+  }
 }
