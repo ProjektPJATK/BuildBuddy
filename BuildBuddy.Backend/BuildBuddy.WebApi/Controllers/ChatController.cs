fc244fa6da69de39d517ba550b86f2b006568cc8
Mateusz Placha <s25946@pjwstk.edu.pl>
2025-01-30 19:21:00 +0100

    Test

--- BuildBuddy.Backend/BuildBuddy.WebApi/Controllers/ChatController.cs
+++ BuildBuddy.Backend/BuildBuddy.WebApi/Controllers/ChatController.cs
@@ -31,4 +31,11 @@ public class ChatController : ControllerBase
         await _chatService.ResetReadStatus(conversationId, userId);
         return Ok();
     }
+    
+    [HttpGet]
+    [Route("Test")]
+    public IActionResult Test()
+    {
+        return Ok("Test");
+    }
 }
